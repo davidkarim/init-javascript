3df8cdb3e983b929b1dc2c399535c4374ecd8635
David Karim <davidkarim@gmail.com>
2023-07-29 15:09:27 -0400

    Create main.js

---
+++ hello-world/main.js
@@ -0,0 +1,15 @@
+// This is a simple JavaScript file that prints "Hello, World!" to the console.
+// Save this code in a file with a .js extension, e.g., hello.js
+
+// Define a function to print the message
+function printHelloWorld(name) {
+    console.log("Hello, " + name + "!");
+}
+  
+// Call the function to print the message
+printHelloWorld("David");
+console.log("Current working directory: " + process.cwd());
+
+// Generate a random number between 1 and 100 (inclusive)
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log("Random number between 1 and 100: " + randomNumber);
